1230983919fd603ed014dc69a75663b06c4e7fca
shockpast <shockpast@gmail.com>
2023-05-02 05:11:05 +0300

    typo

--- src/util.rs
+++ src/util.rs
@@ -3,19 +3,19 @@ use std::process::Command;
 use regex::Regex;
 
 pub fn secure_filename<S: AsRef<str>>(input: S) -> String {
-    let replacemant = "_";
+    let replacement = "_";
 
     let reserved = Regex::new("[<>:\"/\\\\|?*\u{0000}-\u{001F}\u{007F}\u{0080}-\u{009F}]+").unwrap();
     let windows_reserved = Regex::new("^(con|prn|aux|nul|com\\d|lpt\\d)$").unwrap();
     let outer_periods = Regex::new("^\\.+|\\.+$").unwrap();
 
-    let input = reserved.replace_all(input.as_ref(), replacemant);
-    let input = outer_periods.replace_all(input.as_ref(), replacemant);
+    let input = reserved.replace_all(input.as_ref(), replacement);
+    let input = outer_periods.replace_all(input.as_ref(), replacement);
 
     let mut result = input.into_owned();
 
     if windows_reserved.is_match(result.as_str()) {
-        result.push_str(replacemant);
+        result.push_str(replacement);
     }
 
     return result
