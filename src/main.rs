8e27cbe0791b7962b01f7ea1792e5cb04ae7e8a1
shockpast <shockpast@gmail.com>
2023-05-07 17:55:20 +0300

    increased payload size to 100mb (prev. 50mb)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod util;
 
 use dotenv::dotenv;
 use actix_files::Files;
-use actix_multipart::form::tempfile::TempFileConfig;
+use actix_multipart::form::{tempfile::TempFileConfig, MultipartFormConfig};
 use actix_web::{web, App, HttpServer, http::KeepAlive};
 
 #[actix_web::main]
@@ -15,6 +15,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .app_data(TempFileConfig::default().directory("tmp"))
+            .app_data(MultipartFormConfig::default().total_limit(100*1024*1024)) // 100mb
             .service(web::resource("/upload").route(web::post().to(tools::sharex::save_file)))
             .service(web::resource("/shorten").route(web::post().to(tools::sharex::shorten_url)))
             .service(web::resource("/s/{id}").route(web::get().to(tools::sharex::lookup_url)))
